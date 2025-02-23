8ce87fc4caf629590e9c52581444e9de06b3fd49
Yaotecatl Martinez <yaotecatl.martinez@oracle.com>
2025-02-23 16:49:47 -0600

    fix: added danger support in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser, // Agrega los globals del navegador
+        danger: "readonly", // Indica que 'danger' es una variable global de solo lectura
+        fail: "readonly",   // Indica que 'fail' es una variable global de solo lectura
+        warn: "readonly",   // Indica que 'warn' es una variable global de solo lectura
+        message: "readonly", // Indica que 'message' es una variable global de solo lectura
+      },
+    },
+  },
+  {
+    rules: {
+      // Tus reglas personalizadas
+    },
+  },
   pluginJs.configs.recommended,
 ];
